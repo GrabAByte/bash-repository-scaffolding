2a910f114e2271f5b78b1990206dcd6bdf254378
Tom Richards <twr.devops@gmail.com>
2024-06-03 14:55:41 +0100

    updates

--- scaffold.sh
+++ scaffold.sh
@@ -5,13 +5,21 @@ working_directory=$(pwd)
 while getopts ":t:s" opt; do
   case $opt in
     t)
-      target_directory="$OPTARG";;
+      target_directory="$OPTARG"
+      ;;
     s)
-      target_structure="$OPTARG";;
+      target_structure="$OPTARG"
+      ;;
+    *)
+      echo "error: unknown argument"
+      echo "usage: ./scaffold.sh -t ~/my-new-repository -s docker"
+      exit 1
+      ;;
   esac
 done
 
 source helpers/directories.sh
+source helpers/files.sh
 
 ## Create target directory if it doesnt exist
 if [ ! -d "${target_directory}" ]; then
